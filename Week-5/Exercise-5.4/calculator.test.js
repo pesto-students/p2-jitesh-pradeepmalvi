54eca6b509498a7ef6c174760e9c3dc00aed8d37
pradeepmalvi <p.malviyaaa@gmail.com>
2022-06-17 23:32:06 +0530

    issue fixed regarding test cases

--- Week-5/Exercise-5.4/calculator.test.js
+++ Week-5/Exercise-5.4/calculator.test.js
@@ -4,15 +4,9 @@ describe("Calculator tests", () => {
   test("addition 1 + 2 should return 3", () => {
     expect(mathOperations.add(1, 2)).toBe(3);
   });
-});
-
-describe("Calculator tests", () => {
   test("subtraction 2 - 2 should return 0", () => {
     expect(mathOperations.sub(2, 2)).toBe(0);
   });
-});
-
-describe("Calculator tests", () => {
   test("multiplication 2 * 2 should return 4", () => {
     expect(mathOperations.multi(2, 2)).toBe(4);
   });
