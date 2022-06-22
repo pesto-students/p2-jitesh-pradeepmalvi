9a8f49e1d6ff055cec22d322df1ea6b93ff91112
pradeepmalvi <p.malviyaaa@gmail.com>
2022-06-22 15:01:26 +0530

    sort an array exercise updated

--- Week-6/Exercise-6.3/script.js
+++ Week-6/Exercise-6.3/script.js
@@ -1,5 +1,28 @@
-const sortAscending = (nums) => {
-  return nums.sort((a, b) => a - b)
-}
+const sortAscending = nums => {
+  let low = 0;
+  let high = nums.length - 1;
+  let mid = 0;
 
-console.log(sortAscending([0, 2, 1, 2, 0]))
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      let temp = nums[mid];
+      nums[mid] = nums[low];
+      nums[low] = temp;
+      low++;
+      mid++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else if (nums[mid] === 2) {
+      let temp = nums[mid];
+      nums[mid] = nums[high];
+      nums[high] = temp;
+      high--;
+    }
+  }
+
+  return nums;
+};
+
+console.log(
+  sortAscending([0, 2, 1, 2, 0, 2, 2, 2, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0])
+);
