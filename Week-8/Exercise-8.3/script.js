d30ea55f5768ddfb51d2e7824d5ecc5b3e26328f
pradeepmalvi <p.malviyaaa@gmail.com>
2022-07-04 17:37:10 +0530

    Binary Tree Level Order Traversal - done

---
+++ Week-8/Exercise-8.3/script.js
@@ -0,0 +1,51 @@
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let root = null;
+
+function createBSTRecur(root, key) {
+  if (root === null) {
+    root = new TreeNode(key);
+    return root;
+  }
+
+  if (key < root.value) {
+    root.left = createBSTRecur(root.left, key);
+  } else if (key > root.value) {
+    root.right = createBSTRecur(root.right, key);
+  }
+
+  return root;
+}
+
+function createBST(arr) {
+  for (let a of arr) {
+    root = createBSTRecur(root, a);
+  }
+}
+
+var levelOrder = function (root) {
+  let queue = [root],
+    levelArr = [];
+  while (queue.length) {
+    let childArr = [],
+      valueArr = [];
+    for (const node of queue) {
+      if (!node) continue;
+      valueArr.push(node.value);
+      childArr.push(node.left);
+      childArr.push(node.right);
+    }
+    if (valueArr.length) levelArr.push(valueArr);
+    queue = childArr;
+  }
+  return levelArr;
+};
+
+createBST([3, 9, 20, null, null, 15, 7]);
+console.log(levelOrder(root));
